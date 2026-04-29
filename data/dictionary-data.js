9aab4304cbd3c767d51c7cd5de887966589387a4
shito1050 <shito0115@ezweb.ne.jp>
2026-04-29 22:20:27 +0900

    Update dictionary-data.js

--- data/dictionary-data.js
+++ data/dictionary-data.js
@@ -1,9 +1,12 @@
 window.dictionaryData = [
   {
+    id: "tenkai",
     term: "展開",
     kana: "てんかい",
     yomi: "tenkai",
-    description: "式を変形して，かっこのない形にすること．",
-    url: "dictionary/tenkai.html"
+    group: "て",
+    type: "定義",
+    shortDescription: "式を変形して，かっこのない形にすること．",
+    description: "多項式の積を，単項式の和に変形すること．雑に言うと，カッコがない形にすること．"
   }
 ];
