2fa12bd69f130fa4e226c90bf2c62414d7d7187f
shito1050 <shito0115@ezweb.ne.jp>
2026-04-29 23:16:27 +0900

    Update dictionary-data.js

--- data/dictionary-data.js
+++ data/dictionary-data.js
@@ -1,12 +1,12 @@
 window.dictionaryData = [
-  {
-    id: "tenkai",
-    term: "展開",
-    kana: "てんかい",
-    yomi: "tenkai",
-    group: "て",
-    type: "定義",
-    shortDescription: "式を変形して，かっこのない形にすること．",
-    description: "多項式の積を，単項式の和に変形すること．雑に言うと，カッコがない形にすること．"
-  }
+{
+  id: "tenkai",
+  term: "展開",
+  kana: "てんかい",
+  yomi: "tenkai",
+  group: "て",
+  type: "定義",
+  shortDescription: "かっこのある式を，かっこのない形に変形すること．",
+  description: "かっこのある式を，分配法則などを用いて，かっこのない形に変形すること．"
+}
 ];
