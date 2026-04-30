d504bef10c609cb2d7daaef0c1880f2c56c68821
shito1050 <shito0115@ezweb.ne.jp>
2026-04-30 22:43:45 +0900

    Update lessons-data.js

--- data/lessons-data.js
+++ data/lessons-data.js
@@ -1,1 +1,190 @@
-
+const LESSONS_DATA = [
+  {
+    id: "math1",
+    title: "数学I",
+    units: [
+      {
+        id: "suutoshiki",
+        title: "数と式",
+        path: "lessons/math1/suutoshiki/",
+        lessons: [
+          {
+            id: "shiki-keisan",
+            title: "式の計算",
+            path: "lessons/math1/suutoshiki/shiki-keisan.html"
+          },
+          {
+            id: "takoushiki-kahou-genpou",
+            title: "多項式の加法と減法",
+            path: "lessons/math1/suutoshiki/takoushiki-kahou-genpou.html"
+          },
+          {
+            id: "takoushiki-jouhou",
+            title: "多項式の乗法",
+            path: "lessons/math1/suutoshiki/takoushiki-jouhou.html"
+          },
+          {
+            id: "tenkai-kufuu",
+            title: "工夫して展開する",
+            path: "lessons/math1/suutoshiki/tenkai-kufuu.html"
+          },
+          {
+            id: "insubunkai",
+            title: "因数分解",
+            path: "lessons/math1/suutoshiki/insubunkai.html"
+          }
+        ]
+      },
+      {
+        id: "shuugou-meidai",
+        title: "集合と命題",
+        path: "lessons/math1/shuugou-meidai/",
+        lessons: []
+      },
+      {
+        id: "niji-kansuu",
+        title: "二次関数",
+        path: "lessons/math1/niji-kansuu/",
+        lessons: []
+      },
+      {
+        id: "zukei-keiryou",
+        title: "図形と計量",
+        path: "lessons/math1/zukei-keiryou/",
+        lessons: []
+      },
+      {
+        id: "data-bunseki",
+        title: "データの分析",
+        path: "lessons/math1/data-bunseki/",
+        lessons: []
+      }
+    ]
+  },
+  {
+    id: "mathA",
+    title: "数学A",
+    units: [
+      {
+        id: "baainokazu-kakuritsu",
+        title: "場合の数と確率",
+        path: "lessons/mathA/baainokazu-kakuritsu/",
+        lessons: []
+      },
+      {
+        id: "zukei-seishitsu",
+        title: "図形の性質",
+        path: "lessons/mathA/zukei-seishitsu/",
+        lessons: []
+      }
+    ]
+  },
+  {
+    id: "math2",
+    title: "数学II",
+    units: [
+      {
+        id: "shiki-shoumei",
+        title: "式と証明",
+        path: "lessons/math2/shiki-shoumei/",
+        lessons: []
+      },
+      {
+        id: "fukusosuu-houteishiki",
+        title: "複素数と方程式",
+        path: "lessons/math2/fukusosuu-houteishiki/",
+        lessons: []
+      },
+      {
+        id: "zukei-houteishiki",
+        title: "図形と方程式",
+        path: "lessons/math2/zukei-houteishiki/",
+        lessons: []
+      },
+      {
+        id: "sankaku-kansuu",
+        title: "三角関数",
+        path: "lessons/math2/sankaku-kansuu/",
+        lessons: []
+      },
+      {
+        id: "shisuu-taisuu",
+        title: "指数関数・対数関数",
+        path: "lessons/math2/shisuu-taisuu/",
+        lessons: []
+      },
+      {
+        id: "bibun-sekibun",
+        title: "微分積分",
+        path: "lessons/math2/bibun-sekibun/",
+        lessons: []
+      }
+    ]
+  },
+  {
+    id: "mathB",
+    title: "数学B",
+    units: [
+      {
+        id: "suuretsu",
+        title: "数列",
+        path: "lessons/mathB/suuretsu/",
+        lessons: []
+      },
+      {
+        id: "toukei",
+        title: "統計的な推測",
+        path: "lessons/mathB/toukei/",
+        lessons: []
+      }
+    ]
+  },
+  {
+    id: "math3",
+    title: "数学III",
+    units: [
+      {
+        id: "kyokugen",
+        title: "極限",
+        path: "lessons/math3/kyokugen/",
+        lessons: []
+      },
+      {
+        id: "bibunhou",
+        title: "微分法",
+        path: "lessons/math3/bibunhou/",
+        lessons: []
+      },
+      {
+        id: "sekibunhou",
+        title: "積分法",
+        path: "lessons/math3/sekibunhou/",
+        lessons: []
+      }
+    ]
+  },
+  {
+    id: "mathC",
+    title: "数学C",
+    units: [
+      {
+        id: "vector",
+        title: "ベクトル",
+        path: "lessons/mathC/vector/",
+        lessons: []
+      },
+      {
+        id: "heimenjou-kyokusen",
+        title: "平面上の曲線",
+        path: "lessons/mathC/heimenjou-kyokusen/",
+        lessons: []
+      },
+      {
+        id: "fukusosuu-heimen",
+        title: "複素数平面",
+        path: "lessons/mathC/fukusosuu-heimen/",
+        lessons: []
+      }
+    ]
+  }
+];
