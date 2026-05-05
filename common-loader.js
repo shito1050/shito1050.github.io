358af3e5892b2015228ce70bcc90c98dd45ebbef
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 08:17:12 +0900

    Update common-loader.js

--- common-loader.js
+++ common-loader.js
@@ -7,9 +7,28 @@
   const scriptSrc = currentScript ? currentScript.getAttribute("src") : "common-loader.js";
   const prefix = scriptSrc.replace(/common-loader\.js$/, "");
 
+  function loadInlineScript(code) {
+    document.write("<script>" + code + "<\/script>");
+  }
+
   function loadScript(path) {
     document.write('<script src="' + prefix + path + '"><\/script>');
   }
+
+  function loadExternalScript(src) {
+    document.write('<script src="' + src + '"><\/script>');
+  }
+
+  loadInlineScript(`
+    window.MathJax = {
+      tex: {
+        inlineMath: [['\\\\(', '\\\\)']],
+        displayMath: [['\\\\[', '\\\\]']]
+      }
+    };
+  `);
+
+  loadExternalScript("https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js");
 
   loadScript("data/dictionary-data.js");
   loadScript("data/lessons-data.js");
