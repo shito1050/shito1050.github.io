b4d3a8120ca10dc14235722cd23d82a70aad265c
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 10:45:06 +0900

    Create differentiation.js

---
+++ data/dictionary/math3/differentiation.js
@@ -0,0 +1,25 @@
+window.dictionaryData = window.dictionaryData || [];
+
+window.dictionaryData.push(
+  {
+    id: "inkansuu",
+    term: "陰関数",
+    kana: "いんかんすう",
+    yomi: "inkansuu",
+    type: "定義",
+    subject: "数学III",
+    unit: "微分法",
+    unitIds: ["math3_differentiation"],
+    shortDescription: "$F(x,y)=0$のように，$x$と$y$の関係式によって表された関数のこと．",
+    description: "$F(x,y)=0$のように，$x$と$y$の関係式によって表された関数のこと．局所的にみれば，$y$は$x$の関数になっている．",
+    bodyHtml: `
+      <p>
+        $F(x,y)=0$のように，$x$と$y$の関係式によって表された関数を陰関数という．
+      </p>
+      <p>
+        たとえば，$x^2+y^2=1$は，$y$を$x$の式として明示していないが，$x$と$y$の関係を表している．
+      </p>
+    `,
+    relatedIds: []
+  }
+);
