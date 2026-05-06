f68e9185edb6144a671a83116ecba34cb03da1cb
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 13:38:53 +0900

    Create entrance-problems-index.js

---
+++ data/entrance-problems/entrance-problems-index.js
@@ -0,0 +1,5 @@
+window.entranceProblems = [
+  ...(window.entranceProblemsMath1NumbersAndExpressions || []),
+  ...(window.entranceProblemsMath2TrigonometricFunctions || []),
+  ...(window.entranceProblemsMath3Limits || [])
+];
