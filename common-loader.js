f563a60ea02352f4c230588cf641a9383f06254d
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 23:20:53 +0900

    Update common-loader.js

--- common-loader.js
+++ common-loader.js
@@ -6,7 +6,7 @@
   const currentScript = document.currentScript;
   const scriptSrc = currentScript ? currentScript.getAttribute("src") : "common-loader.js";
   const prefix = scriptSrc.replace(/common-loader\.js$/, "");
-  const version = "20260506-18";
+  const version = "20260506-19";
 
   function addVersion(path) {
     if (path.includes("?")) {
@@ -83,6 +83,7 @@
   loadScript("js/common-path.js");
   loadScript("js/sidebar.js");
   loadScript("js/home.js");
+  loadScript("js/page-title.js");
   loadScript("js/dictionary.js");
   loadScript("js/lessons.js");
   loadScript("js/learned-range.js");
