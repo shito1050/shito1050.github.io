b54c7c32b7e304d1451c9cd4b45f10e452509951
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 11:31:54 +0900

    Update math1-suutoshiki.js

--- data/practice/math1-suutoshiki.js
+++ data/practice/math1-suutoshiki.js
@@ -101,24 +101,24 @@ window.practiceProblems.push(
         </p>
 
         <p>
-          (3)何が\\(\\sqrt{a}\\)で何が\\(\\sqrt{b}\\)かをみつけるのに苦労します．やはり根号の前の“2”が肝になりそうなので，ひとまず\\(2x-2\\sqrt{x^2-1}\\)として眺めましょう．意味ありげな\\(x^2-1\\)に注目して因数分解してみると，\\(a=x+1,b=x-1\\)とあるのかもと思えば，\\(a+b=2a,\\sqrt{ab}=\\sqrt{x^2-1}\\)で見事ビンゴです．
+          (3)何が\\(\\sqrt{a}\\)で何が\\(\\sqrt{b}\\)かをみつけるのに苦労します．やはり根号の前の“2”が肝になりそうなので，ひとまず\\(2x-2\\sqrt{x^2-1}\\)として眺めましょう．意味ありげな\\(x^2-1\\)に注目して因数分解してみると，\\(a=x+1,b=x-1\\)とみるのかもと思えば，\\(a+b=2a,\\sqrt{ab}=\\sqrt{x^2-1}\\)で見事ビンゴです．
         </p>
       </div>
 
       <p class="problem-label">解答</p>
 
-      <div class="math-block">
-        \\[
+      <div class="math-block" style="text-align: left;">
+        \\(
         \\begin{aligned}
         (1)\\quad \\sqrt{5+2\\sqrt{6}}
         &=\\sqrt{(\\sqrt{2}+\\sqrt{3})^2}\\\\
         &=\\sqrt{2}+\\sqrt{3}
         \\end{aligned}
-        \\]
+        \\)
       </div>
 
-      <div class="math-block">
-        \\[
+      <div class="math-block" style="text-align: left;">
+        \\(
         \\begin{aligned}
         (2)\\quad \\sqrt{2-\\sqrt{3}}
         &=\\sqrt{\\frac{4-2\\sqrt{3}}{2}}\\\\
@@ -126,17 +126,17 @@ window.practiceProblems.push(
         &=\\frac{\\sqrt{3}-\\sqrt{1}}{\\sqrt{2}}\\\\
         &=\\frac{\\sqrt{6}-\\sqrt{2}}{2}
         \\end{aligned}
-        \\]
+        \\)
       </div>
 
-      <div class="math-block">
-        \\[
+      <div class="math-block" style="text-align: left;">
+        \\(
         \\begin{aligned}
         (3)\\quad \\sqrt{x-\\sqrt{x^2-1}}
         &=\\frac{\\sqrt{2x-2\\sqrt{(x+1)(x-1)}}}{\\sqrt{2}}\\\\
         &=\\frac{\\sqrt{x+1}-\\sqrt{x-1}}{\\sqrt{2}}
         \\end{aligned}
-        \\]
+        \\)
       </div>
     `
   }
