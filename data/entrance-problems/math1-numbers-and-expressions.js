440bb03df5fc6683603801be57846f5eb5b7cfca
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 13:37:39 +0900

    Create math1-numbers-and-expressions.js

---
+++ data/entrance-problems/math1-numbers-and-expressions.js
@@ -0,0 +1,18 @@
+window.entranceProblemsMath1NumbersAndExpressions = [
+  {
+    id: "entrance-math1-numbers-and-expressions-001",
+    unitIds: ["math1_numbers_and_expressions"],
+    questionHtml: "\\(\\sqrt{9}\\) の値はどれか．",
+    choices: ["\\(3\\)", "\\(-3\\)", "\\(\\pm 3\\)", "\\(9\\)"],
+    correctIndex: 0,
+    explanationHtml: "\\(\\sqrt{9}\\) は，2乗して9になる正の数なので，\\(3\\) である．"
+  },
+  {
+    id: "entrance-math1-numbers-and-expressions-002",
+    unitIds: ["math1_numbers_and_expressions"],
+    questionHtml: "\\(2^3\\) の値はどれか．",
+    choices: ["\\(5\\)", "\\(6\\)", "\\(8\\)", "\\(9\\)"],
+    correctIndex: 2,
+    explanationHtml: "\\(2^3=2\\times2\\times2=8\\) である．"
+  }
+];
