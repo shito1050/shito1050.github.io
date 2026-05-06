6d29b2a1c85146a1cc84316f8023388c52f0df70
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 13:38:05 +0900

    Create math2-trigonometric-functions.js

---
+++ data/entrance-problems/math2-trigonometric-functions.js
@@ -0,0 +1,10 @@
+window.entranceProblemsMath2TrigonometricFunctions = [
+  {
+    id: "entrance-math2-trigonometric-functions-001",
+    unitIds: ["math2_trigonometric_functions"],
+    questionHtml: "\\(\\sin 0\\) の値はどれか．",
+    choices: ["\\(0\\)", "\\(1\\)", "\\(-1\\)", "\\(\\frac{1}{2}\\)"],
+    correctIndex: 0,
+    explanationHtml: "\\(\\sin 0=0\\) である．"
+  }
+];
