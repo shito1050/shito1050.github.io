2b686eb5a7098d21095f23ac2d2d4e1e340e254d
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 13:38:27 +0900

    Create math3-limits.js

---
+++ data/entrance-problems/math3-limits.js
@@ -0,0 +1,10 @@
+window.entranceProblemsMath3Limits = [
+  {
+    id: "entrance-math3-limits-001",
+    unitIds: ["math3_limits"],
+    questionHtml: "\\(\\lim_{x\\to 0}x\\) の値はどれか．",
+    choices: ["\\(0\\)", "\\(1\\)", "\\(\\infty\\)", "存在しない"],
+    correctIndex: 0,
+    explanationHtml: "\\(\\lim_{x\\to 0}x=0\\) である．"
+  }
+];
