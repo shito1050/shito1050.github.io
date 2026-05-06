1142bfe396c36e38853b48fe36efefa4221c1059
shito1050 <shito0115@ezweb.ne.jp>
2026-05-06 10:37:34 +0900

    Create numbers-and-expressions.js

---
+++ data/dictionary/math1/numbers-and-expressions.js
@@ -0,0 +1,22 @@
+window.dictionaryData = window.dictionaryData || [];
+
+window.dictionaryData.push(
+  {
+    id: "tenkai",
+    term: "展開",
+    kana: "てんかい",
+    yomi: "tenkai",
+    type: "定義",
+    subject: "数学I",
+    unit: "数と式",
+    unitIds: ["math1_numbers_and_expressions"],
+    shortDescription: "かっこのある式を，かっこのない形に変形すること．",
+    description: "かっこのある式を，分配法則などを用いて，かっこのない形に変形すること．",
+    bodyHtml: `
+      <p>
+        かっこのある式を，分配法則などを用いて，かっこのない形に変形することを展開という．
+      </p>
+    `,
+    relatedIds: []
+  }
+);
