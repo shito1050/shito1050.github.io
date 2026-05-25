ab80e11efdd43ea6ab8b4e22eb426b94325543c0
shito1050 <shito0115@ezweb.ne.jp>
2026-05-25 21:45:48 +0900

    Update polynomial-root-square-001.js

--- data/practice/math2/various-expressions/polynomial-root-square-001.js
+++ data/practice/math2/various-expressions/polynomial-root-square-001.js
@@ -3,13 +3,14 @@ window.practiceProblems = window.practiceProblems || [];
 window.practiceProblems.push(
   {
     id: "polynomial-root-square-001",
-    title: "数学II_いろいろな式_方程式をつくる_001",
+    order: "2-01-0001",
+    title: "数学II_いろいろな式_方程式をつくる（応用）",
+    listTitle: "数学II_いろいろな式_方程式をつくる（応用）",
     subject: "数学II",
     unit: "いろいろな式",
     topic: "方程式をつくる",
     unitIds: ["math2_various_expressions"],
     difficulty: 3,
-    order: "2-01-0001",
     questionHtml: `
       <p>
         方程式 \\(x^{11}+17x+5=0\\) の解を \\(\\alpha_1,\\alpha_2,\\alpha_3,\\cdots,\\alpha_{11}\\) とする．
@@ -21,16 +22,20 @@ window.practiceProblems.push(
       </p>
 
       <p class="indent-paragraph">
-        (ii) 方程式 \\(f(x)=0\\) は，\\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\) を解にもつ．
+        (ii) 方程式 \\(f(x)=0\\) の解は，\\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\) を解にもつ．
       </p>
     `,
     answerHtml: `
-      <p>
-        \\(f(x)=(x-\\alpha_1^2)(x-\\alpha_2^2)\\cdots(x-\\alpha_{11}^2)\\) などから始めると大変です．
-        係数を決めることが目的なのではなく，方程式をつくることが目的だったはずです
-        （厳密には多項式を求めることですが）．
-        \\(x\\) の22次式をつくれば，\\(x^2\\) の11次式です．
-      </p>
+      <div class="thinking-box">
+        <p>
+          \\(f(x)=(x-\\alpha_1^2)(x-\\alpha_2^2)\\cdots(x-\\alpha_{11}^2)\\) などから始めると大変です．
+          係数を決めることが目的なのではなく，方程式をつくることが目的だったはずです
+          （厳密には多項式を求めることですが）．
+          \\(x\\) の22次式をつくれば，\\(x^2\\) の11次式です．
+        </p>
+      </div>
+
+      <p class="problem-label">解答</p>
 
       <p>
         \\(x^{11}+17x+5=0\\) のとき，\\(x^{11}+17x=-5\\) として両辺を2乗すると，
@@ -41,8 +46,7 @@ window.practiceProblems.push(
         (x^{11}+17x)^2=25
         \\]
         \\[
-        \\therefore x^{22}+34x^{12}+289x^2-25=0
-        \\tag{①}
+        \\therefore x^{22}+34x^{12}+289x^2-25=0 \\qquad ①
         \\]
       </div>
 
@@ -62,7 +66,7 @@ window.practiceProblems.push(
       </p>
 
       <p>
-        よって，答えは
+        したがって，答えは
       </p>
 
       <div class="math-block answer">
