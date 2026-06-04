0a1dcbad5f92b32046cdd88319e5d086f10d1a1a
shito1050 <shito0115@ezweb.ne.jp>
2026-06-04 19:28:36 +0900

    Update polynomial-root-square-001.js

--- data/practice/math2/various-expressions/polynomial-root-square-001.js
+++ data/practice/math2/various-expressions/polynomial-root-square-001.js
@@ -13,32 +13,34 @@ window.practiceProblems.push(
     difficulty: 3,
     questionHtml: `
       <p>
-        方程式 \\(x^{11}+17x+5=0\\) の解を \\(\\alpha_1,\\alpha_2,\\alpha_3,\\cdots,\\alpha_{11}\\) とする．
-        11次の多項式 \\(f(x)\\) が次の条件 (i), (ii) を満たすとき，\\(f(x)\\) を求めよ．
+        方程式\\(x^{11}+17x+5=0\\)の解を\\(\\alpha_1,\\alpha_2,\\alpha_3,\\cdots,\\alpha_{11}\\)とする．
+        11次の多項式\\(f(x)\\)が次の条件(i), (ii)を満たすとき，\\(f(x)\\)を求めよ．
       </p>
 
       <p class="indent-paragraph">
-        (i) \\(x^{11}\\) の係数は \\(1\\) である．
+        (i)\\(x^{11}\\)の係数は\\(1\\)である．
       </p>
 
       <p class="indent-paragraph">
-        (ii) 方程式 \\(f(x)=0\\) の解は，\\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\) を解にもつ．
+        (ii)方程式\\(f(x)=0\\)は，\\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\)を解にもつ．
       </p>
     `,
     answerHtml: `
       <div class="thinking-box">
+        <p class="problem-label">考え方</p>
+
         <p>
-          \\(f(x)=(x-\\alpha_1^2)(x-\\alpha_2^2)\\cdots(x-\\alpha_{11}^2)\\) などから始めると大変です．
+          \\(f(x)=(x-\\alpha_1^2)(x-\\alpha_2^2)\\cdots(x-\\alpha_{11}^2)\\)などから始めると大変です．
           係数を決めることが目的なのではなく，方程式をつくることが目的です
           （厳密には多項式を求めることですが）．
-          \\(x\\) の22次式をつくれば，\\(x^2\\) の11次式になります．
+          \\(x\\)の22次式をつくれば，\\(x^2\\)の11次式になります．
         </p>
       </div>
 
       <p class="problem-label">解答</p>
 
       <p>
-        \\(x^{11}+17x+5=0\\) のとき，\\(x^{11}+17x=-5\\) として両辺を2乗すると，
+        \\(x^{11}+17x+5=0\\)のとき，\\(x^{11}+17x=-5\\)として両辺を2乗すると，
       </p>
 
       <div class="math-block">
@@ -46,13 +48,14 @@ window.practiceProblems.push(
         (x^{11}+17x)^2=25
         \\]
         \\[
-        \\therefore x^{22}+34x^{12}+289x^2-25=0 \\qquad ①
+        x^{22}+34x^{12}+289x^2-25=0
+        \\tag*{①}
         \\]
       </div>
 
       <p>
-        となる．この式は当然 \\(\\alpha_1,\\alpha_2,\\alpha_3,\\cdots,\\alpha_{11}\\) を代入して成り立つので，
-        \\(X=x^2\\) とおけば①は
+        となる．この式は当然\\(\\alpha_1,\\alpha_2,\\alpha_3,\\cdots,\\alpha_{11}\\)を代入して成り立つので，
+        \\(X=x^2\\)とおけば①は
       </p>
 
       <div class="math-block">
@@ -62,7 +65,7 @@ window.practiceProblems.push(
       </div>
 
       <p>
-        となり，これは \\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\) を解にもつ，11次の項の係数が1である多項式である．
+        となり，これは\\(\\alpha_1^2,\\alpha_2^2,\\alpha_3^2,\\cdots,\\alpha_{11}^2\\)を解にもつ，11次の項の係数が1である多項式である．
       </p>
 
       <p>
@@ -74,7 +77,6 @@ window.practiceProblems.push(
         f(x)=x^{11}+34x^6+289x-25
         \\]
       </div>
-
     `
   }
 );
